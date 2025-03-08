def6ff772fd3cdbb22da0a0eced73c04a1d26cbd
Miguel Barbosa <miguelbarbosacs@gmail.com>
2025-03-08 15:56:50 -0300

    fix: update client type at assets gateway

--- src/assets/assets.gateway.ts
+++ src/assets/assets.gateway.ts
@@ -4,7 +4,7 @@ import {
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { AssetsService } from './assets.service';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
 import { AssetPresenter } from './asset.presenter';
 
@@ -23,32 +23,36 @@ export class AssetsGateway implements OnGatewayInit {
   }
 
   @SubscribeMessage('join-assets')
-  handleJoinAssets(client: any, payload: { symbols: string[] }) {
+  handleJoinAssets(client: Socket, payload: { symbols: string[] }) {
     if (!payload.symbols?.length) return;
 
-    payload.symbols.forEach((symbol) => client.join(symbol));
+    payload.symbols.forEach((symbol) => {
+      void client.join(symbol);
+    });
     this.logger.log(
       `Client ${client.id} joined assets: ${payload.symbols.join(', ')}`,
     );
   }
   @SubscribeMessage('join-asset')
-  handleJoinAsset(client: any, payload: { symbol: string }) {
-    client.join(payload.symbol);
+  handleJoinAsset(client: Socket, payload: { symbol: string }) {
+    void client.join(payload.symbol);
     this.logger.log(`Client ${client.id} joined asset: ${payload.symbol}`);
   }
 
   @SubscribeMessage('leave-assets')
-  handleLeaveAssets(client: any, payload: { symbols: string[] }) {
+  handleLeaveAssets(client: Socket, payload: { symbols: string[] }) {
     if (!payload.symbols?.length) return;
 
-    payload.symbols.forEach((symbol) => client.leave(symbol));
+    payload.symbols.forEach((symbol) => {
+      void client.leave(symbol);
+    });
     this.logger.log(
       `Client ${client.id} left assets: ${payload.symbols.join(', ')}`,
     );
   }
   @SubscribeMessage('leave-asset')
-  handleLeaveAsset(client: any, payload: { symbol: string }) {
-    client.leave(payload.symbol);
+  handleLeaveAsset(client: Socket, payload: { symbol: string }) {
+    void client.leave(payload.symbol);
     this.logger.log(`Client ${client.id} left asset: ${payload.symbol}`);
   }
 }
