96348e7b90e5381c808a282153a10a8a5b1d199e
Miguel Barbosa <miguelbarbosacs@gmail.com>
2025-03-18 14:59:04 -0300

    feat: add kafka server config

---
+++ src/_cmd/kafka.cmd.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../app.module';
+import { ConfluentKafkaServer } from '../kafka/confluent-kafka-server';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, {
+    strategy: new ConfluentKafkaServer({
+      server: {
+        'bootstrap.servers': 'localhost:9094',
+      },
+      consumer: {
+        allowAutoTopicCreation: true,
+        sessionTimeout: 10000,
+        rebalanceTimeout: 10000,
+      },
+    }),
+  });
+  console.log('Starting Kafka Microservice');
+  await app.listen();
+}
+bootstrap();
